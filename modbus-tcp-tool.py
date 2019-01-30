17e6022e45d16a11b99f91b5f56f0dd92e4ca04b
darboy <974782852@qq.com>
2019-01-30 11:54:36 +0800

    add dynamic layout of di & do

--- modbus-tcp-tool.py
+++ modbus-tcp-tool.py
@@ -1,6 +1,5 @@
 # #!/usr/bin/python3
 # # -*- coding: utf-8 -*-
-
 import sys
 import time
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
@@ -77,47 +76,65 @@ class Example(QWidget):
         self.is_connected = False
         self.is_connect_pressed = False
         self.grid = QGridLayout()
-        self.setLayout(self.grid)    
-        btn1 = QPushButton("open", self)
-        #btn1.move(30, 50)
-
-        btn2 = QPushButton("close", self)
-        #btn2.move(30, 100)
-
-        btn3 = QPushButton("query", self)
-        #btn3.move(30, 150)
-
-        btn1.clicked.connect(self.buttonClicked)            
-        btn2.clicked.connect(self.buttonClicked)
-        btn3.clicked.connect(self.buttonClicked)
+        self.setLayout(self.grid) 
     
-        self.text = "DI状态为:"
-        self.label = QLabel(self.text, self)
-        self.label_address = QLabel("IP地址：", self)
-        #self.label.move(30,300)
+        self.connect_status_text = "连接状态为:"
+        self.connect_status_label = QLabel(self.connect_status_text, self)
 
-        self.tcp_address = QLineEdit("192.168.192.111",self)
-        #self.tcp_address.move(30, 200)
-        btn4 = QPushButton("connect", self)
-        #btn4.move(30, 250)
-        btn5 = QPushButton("disconnect", self)
-        #btn5.move(200, 250)
-        btn4.clicked.connect(self.buttonClicked)
-        btn5.clicked.connect(self.buttonClicked)
+        self.ip_address_label = QLabel("IP地址：", self)
+        self.ip_address_edit = QLineEdit("192.168.192.111",self)
+        #self.ip_address_edit.setFixedSize(100,25)
 
-        self.grid.addWidget(self.label_address, 1, 0)
-        self.grid.addWidget(self.tcp_address, 1, 1)
-        self.grid.addWidget(btn4, 2, 0)
-        self.grid.addWidget(btn5, 2, 1)
-        self.grid.addWidget(self.label, 3, 0)
-        self.grid.addWidget(btn1, 4, 0)
-        self.grid.addWidget(btn2, 4, 1)
-        self.grid.addWidget(btn3, 5, 0)
+        self.do_address_label = QLabel("DO地址：", self)
+        self.do_address_edit = QLineEdit("1",self)
 
+        self.do_num_label = QLabel("DO数量：", self)
+        self.do_num_edit = QLineEdit("8",self)
+
+        self.di_address_label = QLabel("DI地址：", self)
+        self.di_address_edit = QLineEdit("32",self)
+
+        self.di_num_label = QLabel("DI数量：", self)
+        self.di_num_edit = QLineEdit("8",self)
+
+        connect_button = QPushButton("connect", self)
+        disconnect_button = QPushButton("disconnect", self)
+        #connect_button.setFixedSize(75,25)
+        #disconnect_button.setFixedSize(75,25)
+        connect_button.clicked.connect(self.buttonClicked)
+        disconnect_button.clicked.connect(self.buttonClicked)
+     
+        open_button = QPushButton("open", self)
+        close_button = QPushButton("ResetDO", self)
+        #close_button.setFixedSize(75,25)
+        #open_button.setFixedSize(75,25)
+        open_button.clicked.connect(self.buttonClicked)            
+        close_button.clicked.connect(self.buttonClicked)
+
+        self.grid.addWidget(self.ip_address_label, 1, 0)
+        self.grid.addWidget(self.ip_address_edit, 1, 1)
+        self.grid.addWidget(self.do_address_label, 2, 0)
+        self.grid.addWidget(self.do_address_edit, 2, 1)
+        self.grid.addWidget(self.do_num_label, 3, 0)
+        self.grid.addWidget(self.do_num_edit, 3, 1)
+        self.grid.addWidget(self.di_address_label, 4, 0)
+        self.grid.addWidget(self.di_address_edit, 4, 1)
+        self.grid.addWidget(self.di_num_label, 5, 0)
+        self.grid.addWidget(self.di_num_edit, 5, 1)
+        self.grid.addWidget(connect_button, 6, 0)
+        self.grid.addWidget(disconnect_button, 6, 1)
+
+        self.grid.addWidget(self.connect_status_label, 7, 0)
+        self.grid.addWidget(open_button, 8, 0)
+        self.grid.addWidget(close_button, 8, 1)
+        self.grid.setSpacing(45)
         # self.statusBar()
         # self.resize(550,400)
         #self.setGeometry(300, 300, 400, 400)
         self.setWindowTitle('MODBUS_TCP_TOOLKIT')
+        # self.setWindowOpacity(0.9) # 设置窗口透明度
+        # self.setAttribute(Qt.WA_TranslucentBackground) # 设置窗口背景透明
+        #self.setWindowFlag(Qt.FramelessWindowHint) # 隐藏边框
         self.show()
 
 
@@ -129,55 +146,87 @@ class Example(QWidget):
 
     def buttonClicked(self):
         sender = self.sender()
+        self.di_status_label={}
+        self.di_order_label={}
+        self.button={}
         if "open" == sender.text() :
             if self.is_connected == True:
                 self.modbus_master.writeSingleCoilRegister(1,0,1)
-                # self.statusBar().showMessage("打开操作")
-        elif "close" == sender.text():
+        elif "ResetDO" == sender.text():
             if self.is_connected == True:
                 self.modbus_master.writeSingleCoilRegister()
-                # self.statusBar().showMessage("关闭操作")
         elif "query" == sender.text():
             if self.is_connected == True:
                 status = self.modbus_master.readDiscreteRegister(1,32,8)
                 str1 = "DI状态为:" + str(status[0])
-                self.label.setText(str1)
-                # self.statusBar().showMessage("查询操作")
+                self.di_status_label.setText(str1)
         elif "connect" == sender.text():
+            self.ip_address_edit.setEnabled(False)
+            self.di_num_edit.setEnabled(False)
+            self.do_num_edit.setEnabled(False)
+            self.di_address_edit.setEnabled(False)
+            self.do_address_edit.setEnabled(False)
             if self.is_connect_pressed == False:
-                self.modbus_master = modbus_tcp_toolkit(self.tcp_address.text())
+                self.modbus_master = modbus_tcp_toolkit(self.ip_address_edit.text())
                 status = self.modbus_master.readDiscreteRegister(1,32,8)
                 self.is_connect_pressed = True
                 if status == False:
                     self.is_connected = False
-                    # self.statusBar().showMessage("连接失败...")
+                    self.connect_status_label.setText("连接状态为："+"失败")
                 else:
                     self.is_connected = True
+                    self.connect_status_label.setText("连接状态为："+"成功")
+                    self.modbus_master.writeMultipleCoilRegister()
+                    for i in range(1, (int(self.do_num_edit.text())+1)): 
+                        self.button[i]=QPushButton(str(i))
+                        self.button[i].setFixedSize(50,50)
+                        self.grid.addWidget(self.button[i],9,(i-1))
+                        self.button[i].clicked.connect(self.doClicked)  
+                    for i in range(1, (int(self.di_num_edit.text())+1)):
+                        self.di_order_label[i]=QLabel("DI-"+str(i))
+                        self.di_order_label[i].setFixedSize(50,50)
+                        self.grid.addWidget(self.di_order_label[i],10,(i-1))
+                    for i in range(1, (int(self.di_num_edit.text())+1)): 
+                        self.di_status_label[i-1]=QLabel(" ")
+                        self.di_status_label[i-1].setFixedSize(50,50)
+                        self.grid.addWidget(self.di_status_label[i-1],11,(i-1))
+                    # self.di_status_label[1].setText("sdfa")
                     self.thread = work()
                     self.thread.signal.connect(self.updateText)
-                    self.thread.start()
-                    # self.statusBar().showMessage("连接成功...")
+                    self.thread.start()         
         elif "disconnect" == sender.text():
             self.is_connect_pressed = False
             self.is_connected = False
+            self.ip_address_edit.setEnabled(True)
+            self.di_num_edit.setEnabled(True)
+            self.do_num_edit.setEnabled(True)
+            self.di_address_edit.setEnabled(True)
+            self.do_address_edit.setEnabled(True)
+            self.connect_status_label.setText("连接状态为："+"断开")
             #self.grid.removeWidget(self.label)
             #隐藏控件
             #self.label.close()
-            
-
 
     def updateText(self):
         if self.is_connected == True:
-            status = self.modbus_master.readDiscreteRegister(1,32,8)
-            print(status)
-            str1 = "DI状态为:" + str(status[0])
-            self.label.setText(str1)
+            status = self.modbus_master.readDiscreteRegister(1,32,int(self.di_num_edit.text()))
+            # print(status[7])
+            # print(self.di_status_label[3])
+            # str1 = "DI状态为:" + str(status[0])
+            for i in range(1, 9):
+                self.di_status_label[i-1].setText(str(status[i-1]))
+            #self.di_status_label.setText(str1)
             # self.statusBar().showMessage("查询操作")
-        
-        
+    
+    def doClicked(self):
+        sender = self.sender()
+        if self.is_connected == True:
+            print("DO-"+sender.text())
+            self.modbus_master.writeSingleCoilRegister(1,(int(sender.text())-1),1)
 
+        
 class work(QThread):
-    signal = pyqtSignal()    # 括号里填写信号传递的参数
+    signal = pyqtSignal()    # 括号里填写信号传递的参数类型
     def __init__(self):
         super().__init__()
 
@@ -190,9 +239,7 @@ class work(QThread):
             self.signal.emit()    # 发射信号
             time.sleep(1)
 
-
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
